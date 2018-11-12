659e637663a65cebee5ecbe67f0b5b1351c62042
longqianlu <44640628+longqianlu@users.noreply.github.com>
2018-11-12 10:50:48 +0800

    Add files via upload

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,20 @@
 /**
  * Created by web on 2018/11/2.
  */
-$().click(function(){
-    var i=1;
-    return function(){
-        console.log("赞+"+i++);
+var as=document.querySelector(
+    "#tab>li>[data-toggle=tab]"
+);
+for(var a of as){
+    a.onclick=function(){
+        var a=this;
+        var id=a.getAttribute("data-id");//ty-tab1
+        var div=document.querySelector(id);
+        var divs=document.querySelectorAll("#ty-tab-content>div");
+        for(var d of divs){
+            d.style.zIndex="";
+        }
+            d.style.zIndex=10;
     }
-})
+}
 
 
